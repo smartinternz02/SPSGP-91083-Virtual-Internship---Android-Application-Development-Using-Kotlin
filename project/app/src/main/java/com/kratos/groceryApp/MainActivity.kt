a1a389bc393cbd24a655b252e7c23582c7b2fc07
Chandra Prakash <scp.thedevil@gmail.com>
2022-09-14 19:40:45 +0530

    Layouts for Lists
    Signed-off-by: Chandra Prakash <scp.thedevil@gmail.com>

--- project/app/src/main/java/com/kratos/groceryApp/MainActivity.kt
+++ project/app/src/main/java/com/kratos/groceryApp/MainActivity.kt
@@ -2,10 +2,21 @@ package com.kratos.groceryApp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
+    lateinit var itemsRV : RecyclerView
+    lateinit var addFAB : FloatingActionButton
+    lateinit var list: List<GroceryItems>
+    lateinit var groceryRVAdapter: GroceryRVAdapter
+    lateinit var groceryViewModal: GroceryViewModal
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        itemsRV = findViewById(R.id.idRVItems)
+        addFAB = findViewById(R.id.idFABdd)
+
     }
 }
